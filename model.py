271885aab71a8dc101c6f88cf10473ad6cdb930d
Tianyang Zhao <tyzhao@ucla.edu>
2020-01-15 23:54:34 -0800

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 # Architecture Details for CVPR 19 paper: Multi-Agent Tensor Fusion for Contextual Trajectory Prediction
 # Link: http://openaccess.thecvf.com/content_CVPR_2019/html/Zhao_Multi-Agent_Tensor_Fusion_for_Contextual_Trajectory_Prediction_CVPR_2019_paper.html
 # ArXiv Link: https://arxiv.org/abs/1904.04776
-# Feel free to contact: zhaotianyang@pku.edu.cn; Please include 'MATF' in the email title  
+# Feel free to contact: tyzhao@ucla.edu; Please include 'MATF' in the email title  
 
 ## Code for MATF main architecture
 
